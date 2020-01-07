ee8422f026b7ad224673c0c63b37641b7fd3055c
Mark Halls <mark.halls@gmail.com>
2020-01-07 16:12:22 -0700

    Add initial routers for users and tickets

---
+++ routers/tickets/priorities/priorities-router.js
@@ -0,0 +1,46 @@
+const router = require("express").Router();
+
+const bodyValidation = require(`../../../api/middleware/body-validation`);
+const Priorities = require(`../../../database/models/priorities-model`);
+
+router.get(`/`, (req, res) => {
+  Priorities.find()
+    .then(priorities => res.status(200).json({priorities}))
+    .catch(error => res.status(500).json({ errorMessage: error }));
+});
+
+router.get(`/:id`, (req, res) => {
+  Priorities.find(req.params.id)
+    .then(priority => res.status(200).json({priority}))
+    .catch(error => res.status(500).json({ errorMessage: error }));
+});
+
+router.post(`/`, bodyValidation([`name`, `level`]), (req, res) => {
+  const { name, level } = req.body;
+  Priorities.add({ name, level: parseInt(level) })
+    .then(priority => res.status(201).json({priority}))
+    .catch(error => res.status(500).json({ errorMessage: error }));
+});
+
+router.put(`/:id`, (req, res) => {
+  const { name, level } = req.body;
+  const fieldsToUpdate = Object.keys({ name, level }).forEach(
+    key => obj[key] === undefined && delete obj[key]
+  );
+
+  if (fieldsToUpdate && fieldsToUpdate.level) {
+    fieldsToUpdate.level = parseInt(fieldsToUpdate.level);
+  }
+
+  Priorities.update({ id: req.params.id, ...fieldsToUpdate })
+    .then(priority => res.status(202).json({priority}))
+    .catch(error => res.status(500).json({ errorMessage: error }));
+});
+
+router.delete(`/:id`, (req, res) => {
+  Priorities.delete(req.params.id)
+    .then(priority => res.status(200).json({message: "Deleted Successfully"}))
+    .catch(error => res.status(500).json({ errorMessage: error }));
+});
+
+module.exports = router;
