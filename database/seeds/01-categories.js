e9b0aa7719a1f379b14fb4896e09af1088d6d17e
Mark Halls <mark.halls@gmail.com>
2020-01-07 17:31:40 -0700

    Change truncate to delete

--- database/seeds/01-categories.js
+++ database/seeds/01-categories.js
@@ -1,7 +1,7 @@
 exports.seed = function(knex) {
   // Deletes ALL existing entries
   return knex("categories")
-    .truncate()
+    .del()
     .then(async function() {
       // Inserts seed entries
       return knex("categories").insert([
@@ -14,7 +14,9 @@ exports.seed = function(knex) {
     })
     .then(() => {
       if (process.env.DB_ENV === "production") {
-        knex.raw(`select setval('categories_id_seq', (select max(id) from categories)`);
+        knex.raw(
+          `select setval('categories_id_seq', (select max(id) from categories)`
+        );
       }
     });
 };
