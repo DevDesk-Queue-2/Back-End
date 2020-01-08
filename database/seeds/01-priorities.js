41d234a5d34564987e156f5f74659ed94b6b6d05
Mark Halls <mark.halls@gmail.com>
2020-01-07 17:35:25 -0700

    Change truncate to del

--- database/seeds/01-priorities.js
+++ database/seeds/01-priorities.js
@@ -1,7 +1,7 @@
 exports.seed = function(knex) {
   // Deletes ALL existing entries
   return knex("priorities")
-    .truncate()
+    .del()
     .then(function() {
       // Inserts seed entries
       return knex("priorities").insert([
